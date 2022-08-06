06baf783c84aec09c08729e2c9c8268367dbb25b
Jaidev Upadhya <97837505+jaiupadh@users.noreply.github.com>
2022-08-06 08:36:41 +0000

    Add files via upload

---
+++ VariableInput.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This is a shell script that takes input variable and prints it. 
+
+echo "What is your name ?"
+
+read name
+
+echo "Good Morning $name !"
+
+sleep 1s
+
+echo "You're looking good today $name !"
+
+sleep 1s
+
+echo "Hope you have a wonderful day $name ! Bye..."
